0e8be1947b2500264a930f76d555ca717267334e
MinGWon <shiftminw@gmail.com>
2025-06-09 07:35:34 +0900

    ㄴㄴ

--- src/pages/display.js
+++ src/pages/display.js
@@ -4,7 +4,7 @@ import DashboardContent from '@/components/DashboardContent';
 
 export default function Display() {
   return (
-    <div style={{ zoom: '1.1' }}>
+    <div style={{ zoom: '1.0' }}>
       <DashboardContent title="순창고등학교 학생회장 선거 - 실시간 출구조사" />
     </div>
   );
